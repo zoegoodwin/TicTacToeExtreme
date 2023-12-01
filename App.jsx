1b8ab353ddb2897d993e976da932e2e340c0d70a
Sha <georcia.eugenio@edu.sait.ca>
2023-12-01 15:34:40 -0700

    attempted to implement speed run

--- App.jsx
+++ App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-
-import {StyleSheet} from 'react-native';
-
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { GameModeProvider } from './src/screens/GameMode';
 
 import Home from './src/screens/Home';
 import Instructions from './src/screens/Instructions';
@@ -16,40 +15,42 @@ const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Home"
-          component={Home}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Instructions"
-          component={Instructions}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Game"
-          component={Game}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Difficulty"
-          component={Difficulty}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="ResumeAndExit"
-          component={ResumeAndExit}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="GameOver"
-          component={GameOver}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GameModeProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Home"
+            component={Home}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Instructions"
+            component={Instructions}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Game"
+            component={Game}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Difficulty"
+            component={Difficulty}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="ResumeAndExit"
+            component={ResumeAndExit}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="GameOver"
+            component={GameOver}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GameModeProvider>
   );
 }
 
--- src/screens/Game.jsx
+++ src/screens/Game.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 import GameBoard from '../components/GameBoard';
+import {useGameMode} from '../screens/GameMode';
 
 import MainLayout from '../layouts/MainLayout';
 import IconButton from '../components/Button';
@@ -27,12 +28,17 @@ const calculateWinner = squares => {
   return null;
 };
 
-const Game = ({navigation}) => {
-  const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+const Game = ({ navigation, route }) => {
+  const { initialTime } = route.params || {};
+  const { time, setTime } = useGameMode(initialTime || 5);
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
   const [stepNumber, setStepNumber] = useState(0);
   const xIsNext = stepNumber % 2 === 0;
   const current = history[stepNumber];
   const winner = calculateWinner(current.squares);
+  const [isNewGameFromGameOver, setIsNewGameFromGameOver] = useState(false);
+
+  let isDraw = false;
 
   const handleClick = i => {
     const newHistory = history.slice(0, stepNumber + 1);
@@ -43,7 +49,7 @@ const Game = ({navigation}) => {
     }
 
     currentSquares[i] = xIsNext ? 'X' : 'O';
-    setHistory(newHistory.concat([{squares: currentSquares}]));
+    setHistory(newHistory.concat([{ squares: currentSquares }]));
     setStepNumber(newHistory.length);
   };
 
@@ -53,14 +59,34 @@ const Game = ({navigation}) => {
 
   useEffect(() => {
     const winner = calculateWinner(current.squares);
-    const isDraw = !winner && current.squares.every(square => square !== null);
+    let isDraw;
 
-    if (winner || isDraw) {
-      navigation.navigate('GameOver', {winner, isDraw});
-      setHistory([{squares: Array(9).fill(null)}]);
-      setStepNumber(0);
+    if (!winner) {
+      isDraw = current.squares.every(square => square !== null);
     }
-  }, [current.squares, navigation]);
+
+    if (time === 0) {
+      navigation.navigate('GameOver', { winner: null, isDraw: false });
+      setTime(0);
+    } else if (winner || isDraw) {
+      navigation.navigate('GameOver', { winner, isDraw });
+      setHistory([{ squares: Array(9).fill(null) }]);
+      setStepNumber(0);
+    } else if (time > 0 && xIsNext && !winner && !isDraw && !isNewGameFromGameOver) {
+      const countdownInterval = setInterval(() => {
+        setTime(prevTime => (prevTime > 0 ? prevTime - 1 : 0));
+      }, 1000);
+
+      return () => {
+        clearInterval(countdownInterval);
+      };
+    }
+
+    if (isNewGameFromGameOver) {
+      setTime(initialTime);
+      setIsNewGameFromGameOver(false);
+    }
+  }, [current.squares, navigation, time, xIsNext, winner, setTime, isNewGameFromGameOver, initialTime]);
 
   let status;
 
@@ -76,7 +102,7 @@ const Game = ({navigation}) => {
         <View>
           <Text style={styles.title}>Tic Tac Toe </Text>
           <Text style={styles.status}>{status}</Text>
-
+          <Text style={styles.status}>{`Time Left: ${time === 1 ? '1 second' : time === 0 ? '0 second' : `${time} seconds`}`}</Text>
           <GameBoard squares={current.squares} onPress={handleClick} />
         </View>
         <View style={styles.buttonMenu}>
